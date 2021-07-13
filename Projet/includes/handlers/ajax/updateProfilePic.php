98780d866b74fe23aafd3eab470ebedd58a8602c
yassin-bouchlouch <your_email@example.comnpm>
2021-07-13 02:25:41 +0100

    updateQuery profilepic

--- Projet/includes/handlers/ajax/updateProfilePic.php
+++ Projet/includes/handlers/ajax/updateProfilePic.php
@@ -2,12 +2,7 @@
 include("../../config.php");
 
 
-
-if(isset($_POST['songId'])) {
-	$songId = $_POST['songId'];
-
-
-	$updateQuery = mysqli_query($con, "UPDATE users SET email = '$email' WHERE username='$username'");
+	$updateQuery = mysqli_query($con, "UPDATE users SET profilePic = '$profilePic' WHERE username='$username'");
 	echo "Update successful";
 
 
