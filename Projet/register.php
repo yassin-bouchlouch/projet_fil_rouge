a67cfc52576d0240cfa5ed70d58ccce101c7b0b4
yassin-bouchlouch <your_email@example.comnpm>
2021-07-14 19:06:09 +0100

    add selection style

--- Projet/register.php
+++ Projet/register.php
@@ -80,19 +80,19 @@
 						<?php echo $account->getError(Constants::$usernameCharacters); ?>
 						<?php echo $account->getError(Constants::$usernameTaken); ?>
 						<label for="username">Username</label>
-						<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+						<input id="username" name="username" type="text"  value="<?php getInputValue('username') ?>" required>
 					</p>
 
 					<p>
 						<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 						<label for="firstName">First name</label>
-						<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+						<input id="firstName" name="firstName" type="text" value="<?php getInputValue('firstName') ?>" required>
 					</p>
 
 					<p>
 						<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 						<label for="lastName">Last name</label>
-						<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+						<input id="lastName" name="lastName" type="text"  value="<?php getInputValue('lastName') ?>" required>
 					</p>
 
 					<p>
@@ -100,12 +100,12 @@
 						<?php echo $account->getError(Constants::$emailInvalid); ?>
 						<?php echo $account->getError(Constants::$emailTaken); ?>
 						<label for="email">Email</label>
-						<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+						<input id="email" name="email" type="email"  value="<?php getInputValue('email') ?>" required>
 					</p>
 
 					<p>
 						<label for="email2">Confirm email</label>
-						<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+						<input id="email2" name="email2" type="email"  value="<?php getInputValue('email2') ?>" required>
 					</p>
 
 					<p>
@@ -113,12 +113,12 @@
 						<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
 						<?php echo $account->getError(Constants::$passwordCharacters); ?>
 						<label for="password">Password</label>
-						<input id="password" name="password" type="password" placeholder="Your password" required>
+						<input id="password" name="password" type="password"  required>
 					</p>
 
 					<p>
 						<label for="password2">Confirm password</label>
-						<input id="password2" name="password2" type="password" placeholder="Your password" required>
+						<input id="password2" name="password2" type="password"  required>
 					</p>
 
 					<button type="submit" name="registerButton">SIGN UP</button>
