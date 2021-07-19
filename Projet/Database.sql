c8d0e79967192ef75aec6fa2207c78b0c108ec84
yassin-bouchlouch <your_email@example.comnpm>
2021-07-19 18:53:11 +0100

    update sql file

--- Projet/Database.sql
+++ Projet/Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2021 at 05:27 PM
+-- Generation Time: Jul 19, 2021 at 07:50 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `albums` (
   `id` int(11) NOT NULL,
-  `title` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `title` varchar(250) COLLATE utf8mb4_bin NOT NULL,
   `artist` int(11) NOT NULL,
   `genre` int(11) NOT NULL,
   `artworkPath` varchar(500) CHARACTER SET latin1 NOT NULL
@@ -40,14 +40,19 @@ CREATE TABLE `albums` (
 --
 
 INSERT INTO `albums` (`id`, `title`, `artist`, `genre`, `artworkPath`) VALUES
-(1, 'Bacon and Eggs', 2, 4, 'assets/images/artwork/clearday.jpg'),
-(2, 'Pizza head', 5, 10, 'assets/images/artwork/energy.jpg'),
-(3, 'Summer Hits', 3, 1, 'assets/images/artwork/goinghigher.jpg'),
-(4, 'The movie soundtrack', 2, 9, 'assets/images/artwork/funkyelement.jpg'),
-(5, 'Best of the Worst', 1, 3, 'assets/images/artwork/popdance.jpg'),
-(6, 'Hello World', 3, 6, 'assets/images/artwork/ukulele.jpg'),
-(7, 'Best beats', 4, 7, 'assets/images/artwork/sweet.jpg'),
-(8, 'Rumi', 6, 6, 'assets/images/artwork/rumi.jpg');
+(1, 'Bacon and Eggs', 2, 4, 'clearday.jpg'),
+(2, 'Pizza head', 5, 10, 'energy.jpg'),
+(3, 'Summer Hits', 3, 1, 'goinghigher.jpg'),
+(4, 'The movie soundtrack', 2, 9, 'funkyelement.jpg'),
+(5, 'Best of the Worst', 1, 3, 'lise.jpg'),
+(6, 'Hello World', 3, 6, 'ukulele.jpg'),
+(7, 'Best beats', 4, 7, 'sweet.jpg'),
+(8, 'Rumi', 6, 6, 'rumi.jpg'),
+(39, 'spring', 12, 6, 'dope.jpg'),
+(40, 'summer', 13, 1, 'summer.jpg'),
+(41, 'success', 13, 1, 'headphones.jpg'),
+(42, 'Building the Marcus brand: Goldman?s 2021 vision for its consumer banking product', 15, 13, 'avatars-000303494746-0g25uu-t500x500.jpg'),
+(44, 'Podcast Lahtha | بودكاست لحظة', 16, 6, 'Lahtha_aljazeera_podcasts.jpg');
 
 -- --------------------------------------------------------
 
@@ -71,7 +76,12 @@ INSERT INTO `artists` (`id`, `name`, `Email`) VALUES
 (3, 'Bart Simpson', ''),
 (4, 'Homer', ''),
 (5, 'Bruce Lee', ''),
-(6, 'rowaah', 'rowaah@gmail.com');
+(6, 'rowaah', 'rowaah@gmail.com'),
+(12, 'sivalion', 'boc.yassin@gmail.com'),
+(13, 'yassin', 'bouchlouch.yassin.dev@gmail.com'),
+(14, 'asmae', 'Asmae@gmail.com'),
+(15, 'tearsheet', 'tearsheet@gmail.com'),
+(16, 'AlJazeera', 'Podcasts@AlJazeera.com ');
 
 -- --------------------------------------------------------
 
@@ -167,16 +177,16 @@ CREATE TABLE `podcasts` (
 --
 
 INSERT INTO `podcasts` (`id`, `title`, `artist`, `album`, `genre`, `duration`, `path`, `albumOrder`, `plays`) VALUES
-(1, 'Acoustic Breeze', 1, 5, 8, '2:37', 'assets/music/bensound-acousticbreeze.mp3', 1, 10),
+(1, 'Acoustic Breeze', 1, 5, 8, '2:37', 'assets/music/bensound-acousticbreeze.mp3', 1, 11),
 (2, 'A new beginning', 1, 5, 1, '2:35', 'assets/music/bensound-anewbeginning.mp3', 2, 5),
 (3, 'Better Days', 1, 5, 2, '2:33', 'assets/music/bensound-betterdays.mp3', 3, 11),
 (4, 'Buddy', 1, 5, 3, '2:02', 'assets/music/bensound-buddy.mp3', 4, 13),
-(5, 'Clear Day', 1, 5, 4, '1:29', 'assets/music/bensound-clearday.mp3', 5, 9),
+(5, 'Clear Day', 1, 5, 4, '1:29', 'assets/music/bensound-clearday.mp3', 5, 10),
 (6, 'Going Higher', 2, 1, 1, '4:04', 'assets/music/bensound-goinghigher.mp3', 1, 31),
-(7, 'Funny Song', 2, 4, 2, '3:07', 'assets/music/bensound-funnysong.mp3', 2, 13),
-(8, 'Funky Element', 2, 1, 3, '3:08', 'assets/music/bensound-funkyelement.mp3', 2, 26),
+(7, 'Funny Song', 2, 4, 2, '3:07', 'assets/music/bensound-funnysong.mp3', 2, 14),
+(8, 'Funky Element', 2, 1, 3, '3:08', 'assets/music/bensound-funkyelement.mp3', 2, 27),
 (9, 'Extreme Action', 2, 1, 4, '8:03', 'assets/music/bensound-extremeaction.mp3', 3, 28),
-(10, 'Epic', 2, 4, 5, '2:58', 'assets/music/bensound-epic.mp3', 3, 17),
+(10, 'Epic', 2, 4, 5, '2:58', 'assets/music/bensound-epic.mp3', 3, 19),
 (11, 'Energy', 2, 1, 6, '2:59', 'assets/music/bensound-energy.mp3', 4, 21),
 (12, 'Dubstep', 2, 1, 7, '2:03', 'assets/music/bensound-dubstep.mp3', 5, 22),
 (13, 'Happiness', 3, 6, 8, '4:21', 'assets/music/bensound-happiness.mp3', 5, 3),
@@ -189,13 +199,13 @@ INSERT INTO `podcasts` (`id`, `title`, `artist`, `album`, `genre`, `duration`, `
 (20, 'Of Elias Dream', 4, 7, 3, '4:58', 'assets/music/bensound-ofeliasdream.mp3', 3, 6),
 (21, 'Pop Dance', 4, 7, 2, '2:42', 'assets/music/bensound-popdance.mp3', 2, 10),
 (22, 'Retro Soul', 4, 7, 5, '3:36', 'assets/music/bensound-retrosoul.mp3', 1, 10),
-(23, 'Sad Day', 5, 2, 1, '2:28', 'assets/music/bensound-sadday.mp3', 1, 10),
+(23, 'Sad Day', 5, 2, 1, '2:28', 'assets/music/bensound-sadday.mp3', 1, 11),
 (24, 'Sci-fi', 5, 2, 2, '4:44', 'assets/music/bensound-scifi.mp3', 2, 2),
 (25, 'Slow Motion', 5, 2, 3, '3:26', 'assets/music/bensound-slowmotion.mp3', 3, 4),
 (26, 'Sunny', 5, 2, 4, '2:20', 'assets/music/bensound-sunny.mp3', 4, 18),
 (27, 'Sweet', 5, 2, 5, '5:07', 'assets/music/bensound-sweet.mp3', 5, 14),
 (28, 'Tenderness ', 3, 3, 7, '2:03', 'assets/music/bensound-tenderness.mp3', 4, 15),
-(29, 'The Lounge', 3, 3, 8, '4:16', 'assets/music/bensound-thelounge.mp3 ', 3, 7),
+(29, 'The Lounge', 3, 3, 8, '4:16', 'assets/music/bensound-thelounge.mp3 ', 3, 8),
 (30, 'Ukulele', 3, 3, 9, '2:26', 'assets/music/bensound-ukulele.mp3 ', 2, 18),
 (31, 'Tomorrow', 3, 3, 1, '4:54', 'assets/music/bensound-tomorrow.mp3 ', 1, 9),
 (32, 'الرومي| الحلقة الأولى: وداعُ الذهب', 6, 8, 6, '', 'assets/music/الرومي_ الحلقة الأولى_ وداعُ الذهب.mp3', 5, 13);
@@ -220,7 +230,9 @@ CREATE TABLE `songs` (
 --
 
 INSERT INTO `songs` (`song_id`, `song_mp3`, `song_photo`, `song_date`, `aritst_id`, `song_name`) VALUES
-(1, '1626272277_5781069658789_bensound-moose.mp3', '1626272277_41979217218584_solicode-prototype.jpg', NULL, NULL, 'hope');
+(1, '1626272277_5781069658789_bensound-moose.mp3', '1626272277_41979217218584_solicode-prototype.jpg', NULL, NULL, 'hope'),
+(2, '1626462325_14925027287506_bensound-moose.mp3', '1626462325_41760663615116_vinland-saga-138095.jpg', NULL, NULL, 'spong'),
+(3, NULL, '1626517672_83735973485343_typography_heyeksa.jpg', NULL, NULL, 'hola');
 
 -- --------------------------------------------------------
 
@@ -247,7 +259,8 @@ INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `email`, `passwo
 (1, 'reece-kenney', 'Reece', 'Kenney', 'Reece@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2017-06-28 00:00:00', 'assets/images/profile-pics/head_emerald.png'),
 (2, 'donkey-kong', 'Donkey', 'Kong', 'Dk@yahoo.com', '7c6a180b36896a0a8c02787eeafb0e4c', '2017-06-28 00:00:00', 'assets/images/profile-pics/head_emerald.png'),
 (3, 'yassin', 'Yassin', 'Bouchlouch', 'Bouchlouch.yassin.dev@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-07-10 00:00:00', 'assets/images/profile-pics/yassin.jpg'),
-(4, 'asmae', 'Asmae', 'Hamidouch', 'Asmae@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2021-07-14 00:00:00', 'assets/images/profile-pics/head_emerald.png');
+(10, 'sivalion', 'Azzeddin', 'Bouchlouch', 'Boc.yassin@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-07-15 00:00:00', 'assets/images/profile-pics/head_emerald.png'),
+(11, 'asmae', 'Asmae', 'Hamidouche', 'Asmae@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-07-18 00:00:00', 'assets/images/profile-pics/head_emerald.png');
 
 -- --------------------------------------------------------
 
@@ -335,13 +348,13 @@ ALTER TABLE `yourpodcasts`
 -- AUTO_INCREMENT for table `albums`
 --
 ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `artists`
 --
 ALTER TABLE `artists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `genres`
@@ -371,13 +384,13 @@ ALTER TABLE `podcasts`
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `yourpodcasts`
